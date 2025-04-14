20960be90d329e96bfa8fb4d3b80adedc5ffde48
Belal Elshinnawey <belalshinnawey@gmail.com>
2025-04-14 16:18:02 +0200

    adding realtime template

---
+++ src/relbot_rt/include/RelbotRT.hpp
@@ -0,0 +1,42 @@
+#ifndef RELBOT_RT_HPP
+#define RELBOT_RT_HPP
+
+#include "XenoFrtRosIco.hpp"
+
+#pragma pack (1)    //https://carlosvin.github.io/langs/en/posts/cpp-pragma-pack/#_performance_test
+struct LoggedVariable
+{
+    int this_is_a_int = 0;
+    double this_is_a_double = 100.0;
+    float this_is_a_float = 10.0;
+    char this_is_a_char = 'R';
+    bool this_is_a_bool = false;
+};
+
+#pragma pack(0)
+
+class RelbotRT : public XenoFrtRosIco
+{
+public:
+    RelbotRT(uint cycle_time_freq, uint write_decimator_freq, uint monitor_freq);
+    ~RelbotRT();
+private:
+    XenoFileHandler file;
+    struct LoggedVariable data_to_be_logged;
+
+protected:
+    //Functions
+    int initialising() override;
+    int initialised() override;
+    int run() override;
+    int stopping() override;
+    int stopped() override;
+    int pausing() override;
+    int paused() override;
+    int error() override;
+
+    // current error
+    int current_error = 0;
+};
+
+#endif // RELBOTRT_HPP
